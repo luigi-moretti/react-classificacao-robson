ff2ece4388ea8eda06fb91468ab77e94f38f425f
luigi-moretti <66831232+luigi-moretti@users.noreply.github.com>
2021-01-02 13:13:13 -0300

    Ajustes e início form para salvar

--- src/components/Questionario/Questionario.jsx
+++ src/components/Questionario/Questionario.jsx
@@ -6,6 +6,8 @@ import IdadeGestacional from './IdadeGestacional';
 import NumeroFetos from './NumeroFetos';
 import Paridade from './Paridade';
 import TrabalhoParto from './TrabalhoParto';
+import Classificacao from './Classificacao';
+import Salvar from './Salvar';
 import Finalizacao from './Finalizacao';
 
 
@@ -15,46 +17,56 @@ function Questionario() {
     const [grupo, setGrupo] = useState(0);
 
     useEffect(()=>{
-        if(formAtual === formularios.length-1){
+        if(formAtual === formularios.length-3){
             if(dadosColetados.fetos === 'B'){
-
+                
                 setGrupo(8);
+                //setDados({ ...dadosColetados, ...{grupo:8} });
             }else{
                 if(dadosColetados.apresentacaoFetal === 'C'){
     
                     setGrupo(9);
+                    //setDados({ ...dadosColetados, ...{grupo:9} });
                 } else if(dadosColetados.apresentacaoFetal === 'B'){
                     if(dadosColetados.paridade === 'A'){
         
                         setGrupo(6);
+                        //setDados({ ...dadosColetados, ...{grupo:6} });
                     } else{
         
                         setGrupo(7);
+                        //setDados({ ...dadosColetados, ...{grupo:7} });
                     }
                 } else if(dadosColetados.apresentacaoFetal === 'A'){
                     if(dadosColetados.idadeGestacional === 'B'){
         
                         setGrupo(10);
+                        //setDados({ ...dadosColetados, ...{grupo:10} });
                     }else{
                         if(dadosColetados.paridade === 'C'){
             
                             setGrupo(5);
+                            //setDados({ ...dadosColetados, ...{grupo:5} });
                         } else {
                                 if(dadosColetados.paridade === 'A'){
                                     if(dadosColetados.trabalhoParto === 'A'){
                         
                                         setGrupo(1);
+                                        //setDados({ ...dadosColetados, ...{grupo:1} });
                                     } else{
                         
                                         setGrupo(2);
+                                        //setDados({ ...dadosColetados, ...{grupo:2} });
                                     }
                                 }else {
                                     if(dadosColetados.trabalhoParto === 'A'){
                         
                                         setGrupo(3);
+                                        //setDados({ ...dadosColetados, ...{grupo:3} });
                                     } else{
                         
                                         setGrupo(4);
+                                        //setDados({ ...dadosColetados, ...{grupo:4} });
                                     }
                                 }
                         }
@@ -62,6 +74,10 @@ function Questionario() {
                 }
 
             } 
+        }
+
+        if(formAtual === formularios.length-1){
+            console.log(dadosColetados);
         }
     })
 
@@ -71,10 +87,13 @@ function Questionario() {
         <ApresentacaoFetal aoEnviar={coletarDados} aoVoltar={anterior} />,
         <IdadeGestacional aoEnviar={coletarDados} aoVoltar={anterior} />,
         <TrabalhoParto aoEnviar={coletarDados} aoVoltar={anterior} />,
-        <Finalizacao aoVoltar={anterior} grupo={grupo}/>]
+        <Classificacao aoEnviar={coletarDados} aoVoltar={anterior} dadosColetados={dadosColetados} grupo={grupo}/>,
+        <Salvar aoEnviar={coletarDados} aoVoltar={anterior} />,
+        <Finalizacao aoEnviar={coletarDados} aoVoltar={anterior} />]
+        
 
     function proximo() {
-        setFormAtual(formAtual + 1);
+            setFormAtual(formAtual + 1);
     }
 
     function anterior() {
---
+++ src/components/Questionario/Salvar.jsx
@@ -0,0 +1,93 @@
+import {TextField, Button, Grid, Typography } from '@material-ui/core';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import React, { useState } from 'react';
+
+function Salvar({ aoEnviar, dadosColetados }) {
+    const [nome, setNome] = useState('');
+    const [sobrenome, setSobrenome] = useState('');
+    const [idade, setIdade] = useState('');
+    const [endereco, setEndereco] = useState('');
+    const [numeroEnd, setNumeroEnd] = useState('');
+    const [telefone, setTelefone] = useState('');
+
+    console.log(dadosColetados);
+
+    return (
+        <form
+            style={{ height: "79vh" }}
+            onSubmit={event => {
+                event.preventDefault();
+                aoEnviar({ nome, sobrenome, idade, endereco, numeroEnd, telefone });
+            }}
+        >
+            <Grid
+                container
+                direction="column"
+                justify="space-between"
+                alignItems="flex-start"
+                style={{ height: "100%" }}
+            >
+
+                <Grid item
+                    container
+                    direction="row"
+                    justify="start"
+                    alignItems="flex-start"
+                    spacing={2}
+                >
+                <Grid item xs={12}>
+                    <Typography variant="h4" component="h2">Salvar Dados</Typography>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField onChange={(event) => {
+                        setNome(event.target.value);
+                    }} required label="Nome" variant="outlined" fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField  onChange={(event) => {
+                        setSobrenome(event.target.value);
+                    }}
+                     required label="Sobrenome" variant="outlined" fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                    <TextField onChange={(event) => {
+                        setIdade(event.target.value);
+                    }} required label="Idade" type="number" variant="outlined" fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                    <TextField onChange={(event) => {
+                        setTelefone(event.target.value);
+                    }} label="Telefone" type="number" variant="outlined" fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={10}>
+                    <TextField onChange={(event) => {
+                        setEndereco(event.target.value);
+                    }} label="Endereço" variant="outlined" fullWidth/>
+                </Grid>
+                <Grid item xs={12} sm={2}>
+                    <TextField onChange={(event) => {
+                        setNumeroEnd(event.target.value);
+                    }} label="Número" type="number" variant="outlined" fullWidth/>
+                </Grid>
+                </Grid>
+
+                <Grid
+                    item
+                    container
+                    direction="row"
+                    justify="space-between"
+                >
+                    <Grid item>
+                        <Button variant="contained" color="primary" onClick={event => { event.preventDefault(); }} startIcon={<ArrowBackIosIcon />}>Voltar</Button>
+                    </Grid>
+                    <Grid item>
+                        <Button type="submit" variant="contained" color="primary" endIcon={<ArrowForwardIosIcon />}>Próximo</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </form>
+    );
+
+}
+export default Salvar;
